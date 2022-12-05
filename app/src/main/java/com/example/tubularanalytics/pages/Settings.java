ea3f76210648372a19ccda717808fc92c9b3bddd
Jared Hernandez <jaredx291@gmail.com>
2022-12-05 18:16:41 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/tubularanalytics/pages/Settings.java
@@ -0,0 +1,47 @@
+package edu.floridapoly.mobiledeviceapps.fall22.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        Switch likes = findViewById(R.id.Likes);
+        Switch comments = findViewById(R.id.Comments);
+        Switch views = findViewById(R.id.Views);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor e = settings.edit();
+        likes.setOnClickListener(view -> {
+            e.putBoolean("likes", likes.isChecked());
+            e.commit();
+            String text = "Likes = " +likes.isChecked();
+            Toast toast =Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
+        });
+        comments.setOnClickListener(v -> {
+            e.putBoolean("comments", comments.isChecked());
+            e.commit();
+            String text = "Comments = " +comments.isChecked();
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
+        });
+        views.setOnClickListener(v -> {
+            e.putBoolean("subscribers", views.isChecked());
+            e.commit();
+            String text = "Views = " +views.isChecked();
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
+        });
+
+
+    }
+}
